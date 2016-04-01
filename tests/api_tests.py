de798ab7204cf594704c95dcf2500f82179f87e8
Capi Etheriel <barraponto@gmail.com>
2016-03-31 22:22:28 -0300

    added tests for GET /api/songs

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -6,7 +6,7 @@ try: from urllib.parse import urlparse
 except ImportError: from urlparse import urlparse # Py2 compatibility
 from io import StringIO
 
-import sys; print(list(sys.modules.keys()))
+#import sys; print(list(sys.modules.keys()))
 # Configure our app to use the testing databse
 os.environ["CONFIG_PATH"] = "tuneful.config.TestingConfig"
 
@@ -37,4 +37,29 @@ class TestAPI(unittest.TestCase):
         # Delete test upload folder
         shutil.rmtree(upload_path())
 
+    def test_get_songs(self):
+        file_A = models.File(name='file_A.mp3')
+        song_A = models.Song(file_=file_A)
+        file_B = models.File(name='file_B.mp3')
+        song_B = models.Song(file_=file_B)
 
+        session.add_all([file_A, file_B, song_A, song_B])
+        session.commit()
+
+        response = self.client.get(
+            '/api/songs',
+            headers=[("Accept", "application/json")])
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, 'application/json')
+
+        data = json.loads(response.data.decode('ascii'))
+        self.assertEqual(len(data), 2)
+
+        response_song_A, response_song_B = data
+        self.assertEqual(response_song_A['id'], song_A.id)
+        self.assertEqual(response_song_A['file']['id'], song_A.file_.id)
+        self.assertEqual(response_song_A['file']['name'], song_A.file_.name)
+        self.assertEqual(response_song_B['id'], song_B.id)
+        self.assertEqual(response_song_B['file']['id'], song_B.file_.id)
+        self.assertEqual(response_song_B['file']['name'], song_B.file_.name)
