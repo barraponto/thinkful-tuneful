d0aa6222cdd84133ed1c3e3fd01986102626fdf8
Capi Etheriel <barraponto@gmail.com>
2016-03-31 22:25:46 -0300

    route for GET /api/songs

--- tuneful/api.py
+++ tuneful/api.py
@@ -11,4 +11,11 @@ from tuneful import app
 from .database import session
 from .utils import upload_path
 
+@app.route('/api/songs', methods=['GET'])
+@decorators.accept("application/json")
+def get_songs():
+    data = session.query(models.Song)
 
+    data = data.order_by(models.Song.id)
+    return Response(json.dumps([song.as_dict() for song in data]),
+                    200, mimetype='application/json')
